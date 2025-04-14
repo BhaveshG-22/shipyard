2bb2c97eabd6b55afdf6e68527dbf93559783325
bhaveshgavali2003@gmail.com <bhaveshgavali2003@gmail.com>
2025-04-14 01:48:57 -0400

    Add support for sparse checkout and selective folder deployment using GIT_ROOT

--- build-server/main.sh
+++ build-server/main.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
 export GIT_URL="$GIT_URL"
+export GIT_ROOT="$GIT_ROOT"
 
-git clone "$GIT_URL" /home/app/output
+# Initialize empty git repo
+git init /home/app/output
+cd /home/app/output
 
-exec node script.js
+# Set remote and pull only sparse content
+git remote add origin "$GIT_URL"
+git sparse-checkout init --cone
+git sparse-checkout set "$GIT_ROOT"
+git pull origin main
+
+# Move folder content to root
+mv "$GIT_ROOT"/* .
+rm -rf "$GIT_ROOT"
+
+exec node /home/app/script.js
